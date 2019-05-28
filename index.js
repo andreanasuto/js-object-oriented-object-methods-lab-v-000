ca4b432ce991baae0649259d031f94b2792862fe
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-05-28 15:06:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function boardMembers(name, homestate, training) {
   this.veto = function veto() { return "No, I must disagree" }
   this.approve = function approve () { return "You can do that!" }
   this.charity = function charity () { return "I like to help people."}
-  "I like to help people."
-  "You will see great things from Scuber."
+  this.releasePressStatement = function () { return  "You will see great things from Scuber."}
   this.sayHi = function () { return "Hi, my name is ${name}. I am from {home_state}, and I was trained in {training}."}
 }
