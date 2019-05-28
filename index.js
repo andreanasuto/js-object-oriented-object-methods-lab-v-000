9071481ee3a353aa6d9a562a3d386e012e92ed3e
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-05-28 15:03:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ function boardMembers(name, homestate, training) {
   this.name = name
   this.homeState = home_state
   this.training = training
+  this.veto = function veto() { return "No, I must disagree" }
 }
